af266bffb71e5f407a01ce5eae54e0dde28fb14c
Mayur Dhande <mayurdhande007@gmail.com>
2025-06-02 00:19:16 +0800

    cleanup

--- genai-cohort-2/01-tokenization/main.py
+++
@@ -1,9 +0,0 @@
-import tiktoken 
-enc = tiktoken.encoding_for_model('gpt-4o')
-text = "This is mayur dhande"
-token = enc.encode(text)
-print(token)
-
-tokens=[2500, 382, 1340, 330, 8523, 4102]
-decoded_token=enc.decode(tokens)
-print(decoded_token)
